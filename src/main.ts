8020049916d151d9426b932c2c59139fa53a9a69
kenji7157 <kawake.kk@gmail.com>
2022-03-31 23:44:28 +0900

    アイコン対応

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 // NOTE: グローバルscssファイルの読み込み
 import '@/assets/app.scss'
+// NOTE: アイコンの読み込み
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
 
 const app = createApp(App)
 
